a03a30b1b88ddbc5e04557ea00159a1ce1b07f23
Andrew Zhou <lolserverhascrashed@gmail.com>
2024-08-30 17:41:57 -0700

    letsbuildgpt tutorial completed

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Microservices
+## password_generator
+## countdown_timer
+## hash_function
+## simple web scraping
+## following Let's Build GPT tutorial video by Andrej Karpathy
+https://www.youtube.com/watch?v=kCc8FmEb1nY
+Implement based on Attention Is All You Need Paper
--- transformer.py
+++ transformer.py
@@ -5,14 +5,18 @@ import torch # we use PyTorch
 import torch.nn as nn
 from torch.nn import functional as F
 
-batch_size= 32 # how many independent sequences will we process in parallel
-block_size = 8 # what is the maximum context length for predictions
+# hyperparameters
+batch_size= 16 # how many independent sequences will we process in parallel
+block_size = 32 # what is the maximum context length for predictions
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
+n_head = 4
+n_layer = 4
+dropout = 0.3
 
 torch.manual_seed(1337)
 # read to inspect
@@ -42,7 +46,7 @@ def get_batch(split):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x, y
 
-@torch.no_grad() 
+@torch.no_grad()
 # everything that happens inside this function, we will not call .backward on
 # pytorch can be more efficient with its memory use, don't need to store intermediate variables 
 def estimate_loss():
@@ -86,10 +90,15 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-
+        # projection, deep neural network needs optimizing to perform well
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B,T,C)
+        # apply the projection, linear transformation of the outcome of heads layer
+        out = self.dropout(self.proj(out))
+        return out
 
 class FeedForward(nn.Module):
     """a simple linear layer followed by a non-linearity"""
@@ -97,12 +106,36 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
         return self.net(x)
+
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        # in the paper All You need Is Attention, add & norm is applied after the transformation
+        # now it is more common to apply the layernorm before the transformation
+        # this is called the prenorm formulation
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+    
+    def forward(self, x):
+        # deep neural network needs optimizing to perform well
+        # residual connections / skip connections / addition x = x + ...
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 # bigram language model
 class BigramLanguageModel(nn.Module):
@@ -111,8 +144,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd // 4) # i.e. 4 heads of 8-dimensional self-attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -124,8 +157,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_heads(x) # (B,T,C)
-        x = self.ffwd(x) # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
@@ -166,7 +199,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
 
     # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
+    if iter % eval_interval == 0 or iter == 4999:
         losses = estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
